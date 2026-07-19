f49ea292c63466a04183635b50b0cb5997e2e71a
adedejivictor78-oss <adedejivictor78@gmail.com>
2026-07-19 13:11:14 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        royal: '#2563EB',
+        navy: '#0F172A',
+        bg: '#F8FAFC',
+        success: '#22C55E'
+      }
+    }
+  },
+  plugins: [],
+}
